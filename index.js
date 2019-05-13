1f709cf433ded5bc50e0918d1634c5cb73277781
jpavioli <justin.avioli@gmail.com>
2019-05-13 13:26:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+
+//companyName set as "Scuber" (as a constant)
+const companyName = "Scuber"
+
+//mostProfitable Neighborhood is set as "Chelsea"
+let mostProfitableNeighborhood = "Chelsea"
+
+//companyCeo is equal to "Susan Smith"
+let companyCeo = "Susan Smith"
